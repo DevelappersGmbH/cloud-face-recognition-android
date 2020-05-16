8e5d7a043eb92bdf2e8a871f5085a36451ff7d3e
Elena Stepanova <e.stepanova@enveto.de>
2020-05-16 11:32:26 +0200

    move all visitor data saving to greeting

--- app/src/main/java/de/develappers/facerecognition/GreetingActivity.kt
+++ app/src/main/java/de/develappers/facerecognition/GreetingActivity.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.lifecycleScope
 import de.develappers.facerecognition.database.FRdb
 import de.develappers.facerecognition.database.dao.VisitorDao
 import de.develappers.facerecognition.database.model.LogEntry
-import de.develappers.facerecognition.database.model.RecognisedCandidate
 import de.develappers.facerecognition.database.model.Visitor
 import de.develappers.facerecognition.serviceAI.MicrosoftServiceAI
 import de.develappers.facerecognition.utils.VISITORS_GROUP_ID
@@ -26,17 +25,18 @@ class GreetingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_greeting)
         microsoftServiceAI = MicrosoftServiceAI(this)
-        val match = intent.getSerializableExtra(VISITOR_EXTRA) as Visitor
-        val firstTimeVisitor = intent.getBooleanExtra(VISITOR_FIRST_TIME, false)
-        tvGreeting.text = getString(R.string.greeting, match.lastName)
+        val visitor = intent.getSerializableExtra(VISITOR_EXTRA) as Visitor
+        val firstTime = intent.getBooleanExtra(VISITOR_FIRST_TIME, false)
+        tvGreeting.text = getString(R.string.greeting, visitor.lastName)
 
         lifecycleScope.launch {
             visitorDao = FRdb.getDatabase(application, this).visitorDao()
-            if (firstTimeVisitor) {
-                //also send visitor data to AI services to register
-                val microsoftId = microsoftServiceAI.addNewVisitorToDatabase(VISITORS_GROUP_ID, match.imgPaths.last())
-                match.microsoftId = microsoftId
-                visitorDao.updateVisitor(match)
+            if (firstTime) {
+                //send visitor data to AI services to register
+                val microsoftId = microsoftServiceAI.addNewVisitorToDatabase(VISITORS_GROUP_ID, visitor.imgPaths.last())
+                visitor.microsoftId = microsoftId
+                //save new visitor in database, get visitor id
+                val newVisitorId = visitorDao.insert(visitor)
             }
             //train the database with the new image
             microsoftServiceAI.microsoftTrainPersonGroup(VISITORS_GROUP_ID)
@@ -45,7 +45,7 @@ class GreetingActivity : AppCompatActivity() {
 
         var log = LogEntry()
         log.timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
-        log.microsoft = match.lastName
+        log.microsoft = visitor.lastName
         //TODO: if any data on recognition results in extra, then set log.amazon, log.kairos and so on to this data
         //TODO: add log to the database
 
