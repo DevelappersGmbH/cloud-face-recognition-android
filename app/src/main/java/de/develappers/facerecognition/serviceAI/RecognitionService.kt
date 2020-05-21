f8b912d70d3358e58748d9890b10f9ea35fc4246
Elena Stepanova <e.stepanova@enveto.de>
2020-05-21 17:06:00 +0200

    add a list of service interfaces

--- app/src/main/java/de/develappers/facerecognition/serviceAI/RecognitionService.kt
+++ app/src/main/java/de/develappers/facerecognition/serviceAI/RecognitionService.kt
@@ -7,6 +7,7 @@ import de.develappers.facerecognition.database.model.Visitor
 
 interface RecognitionService {
     suspend fun addNewVisitorToDatabase(personGroupId: String, imgUri: String, visitor: Visitor)
+    suspend fun identifyVisitor(personGroupId: String, imgUri: String): List<Any>
     fun setServiceId(visitor: Visitor, id: String)
     fun setConfidenceLevel(candidate: Any, recognisedCandidate: RecognisedCandidate)
     fun defineLocalIdPath(candidate: Any): String
