4c9ce7ff5e3d0750b9e1b76ed5cd45dfc4515b2d
Elena Stepanova <e.stepanova@enveto.de>
2020-05-22 10:31:07 +0200

    two options in greeting activiy: either new visitor just registered, or recognised visitor

--- app/src/main/java/de/develappers/facerecognition/GreetingActivity.kt
+++ app/src/main/java/de/develappers/facerecognition/GreetingActivity.kt
@@ -6,10 +6,12 @@ import androidx.lifecycle.lifecycleScope
 import de.develappers.facerecognition.database.FRdb
 import de.develappers.facerecognition.database.dao.VisitorDao
 import de.develappers.facerecognition.database.model.LogEntry
+import de.develappers.facerecognition.database.model.RecognisedCandidate
 import de.develappers.facerecognition.database.model.Visitor
 import de.develappers.facerecognition.serviceAI.AmazonServiceAI
 import de.develappers.facerecognition.serviceAI.MicrosoftServiceAI
 import de.develappers.facerecognition.serviceAI.RecognitionService
+import de.develappers.facerecognition.utils.RECOGNISED_CANDIDATE_EXTRA
 import de.develappers.facerecognition.utils.VISITORS_GROUP_ID
 import de.develappers.facerecognition.utils.VISITOR_EXTRA
 import de.develappers.facerecognition.utils.VISITOR_FIRST_TIME
@@ -24,14 +26,12 @@ class GreetingActivity : AppCompatActivity() {
     private lateinit var microsoftServiceAI: MicrosoftServiceAI
     private lateinit var amazonServiceAI: AmazonServiceAI
     private lateinit var visitorDao: VisitorDao
+    private var log = LogEntry()
     private var serviceProviders = mutableListOf<RecognitionService>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_greeting)
-        val visitor = intent.getSerializableExtra(VISITOR_EXTRA) as Visitor
-        val firstTime = intent.getBooleanExtra(VISITOR_FIRST_TIME, false)
-
         //AI services
         microsoftServiceAI = MicrosoftServiceAI(this)
         amazonServiceAI = AmazonServiceAI(this)
@@ -39,14 +39,17 @@ class GreetingActivity : AppCompatActivity() {
         serviceProviders.add(microsoftServiceAI)
         serviceProviders.add(amazonServiceAI)
 
-        tvGreeting.text = getString(R.string.greeting, visitor.lastName)
+        if (intent.hasExtra(VISITOR_EXTRA)) {
+            val visitor = intent.getSerializableExtra(VISITOR_EXTRA) as Visitor
+            tvGreeting.text = getString(R.string.greeting, visitor.lastName)
 
-        lifecycleScope.launch {
-            visitorDao = FRdb.getDatabase(application, this).visitorDao()
-            if (firstTime) {
+            lifecycleScope.launch {
+                visitorDao = FRdb.getDatabase(application, this).visitorDao()
+                //register the visitor within AI services and sets visitor service ID in the datasbe
                 registerVisitorInAIServices(visitor)
                 //save new visitor in database, get visitor id
                 val newVisitorId = visitorDao.insert(visitor)
+                registerNewVisitor(newVisitorId.toString())
 
                 //train the database with the new image, if needed
                 serviceProviders.forEach {
@@ -55,13 +58,29 @@ class GreetingActivity : AppCompatActivity() {
             }
         }
 
+        if (intent.hasExtra(RECOGNISED_CANDIDATE_EXTRA)) {
+            val recognisedCandidate = intent.getSerializableExtra(RECOGNISED_CANDIDATE_EXTRA) as RecognisedCandidate
+            tvGreeting.text = getString(R.string.greeting, recognisedCandidate.visitor.lastName)
+            registerRepeatingVisitor(recognisedCandidate)
+        }
 
-        var log = LogEntry()
+
+    }
+
+    fun registerNewVisitor(visitorId: String) {
         log.timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
-        log.microsoft = visitor.microsoftId
-        //TODO: if any data on recognition results in extra, then set log.amazon, log.kairos and so on to this data
-        //TODO: add log to the database
+        log.truth = visitorId
 
+    }
+
+    fun registerRepeatingVisitor(candidate: RecognisedCandidate) {
+        log.timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        log.microsoft = candidate.microsoft_conf.toString()
+        log.amazon = candidate.amazon_conf.toString()
+        log.face = candidate.face_conf.toString()
+        log.kairos = candidate.kairos_conf.toString()
+        log.luxand = candidate.luxand_conf.toString()
+        log.truth = candidate.visitor.visitorId.toString()
 
     }
 
--- app/src/main/java/de/develappers/facerecognition/database/model/RecognisedCandidate.kt
+++ app/src/main/java/de/develappers/facerecognition/database/model/RecognisedCandidate.kt
@@ -2,11 +2,10 @@ package de.develappers.facerecognition.database.model
 
 import java.io.Serializable
 
-class RecognisedCandidate(): Serializable {
+class RecognisedCandidate(var microsoft_conf: Double = 0.0,
+                          var amazon_conf: Double = 0.0,
+                          var kairos_conf: Double = 0.0,
+                          var face_conf: Double = 0.0,
+                          var luxand_conf: Double = 0.0): Serializable {
     lateinit var visitor: Visitor
-    var microsoft_conf: Double? = 0.0
-    var amazon_conf: Double? = 0.0
-    var kairos_conf: Double? = 0.0
-    var face_conf: Double? = 0.0
-    var luxand_conf: Double? = 0.0
 }
