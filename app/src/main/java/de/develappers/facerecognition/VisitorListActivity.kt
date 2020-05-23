07ec4d7308a5c966c97fb636ee3d957274e26bfc
Elena Stepanova <e.stepanova@enveto.de>
2020-05-23 14:14:11 +0200

    change adapter view

--- app/src/main/java/de/develappers/facerecognition/VisitorListActivity.kt
+++ app/src/main/java/de/develappers/facerecognition/VisitorListActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -23,18 +24,42 @@ import kotlinx.android.synthetic.main.item_visitor.view.*
 
 class VisitorListActivity : AppCompatActivity(), OnVisitorItemClickedListener {
 
-    private lateinit var visitorViewModel: VisitorViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_visitor_list)
         showAlertDialog()
 
-        var adapter = VisitorListAdapter(this, this)
+        val titles = listOf (getString(R.string.microsoft),
+            getString(R.string.amazon),
+            getString(R.string.face),
+            getString(R.string.kairos),
+            getString(R.string.luxand))
+
+        val recognisedCandidates = intent.extras?.get(CANDIDATES_EXTRA) as List<RecognisedCandidate>
+
+        titles.forEach{title ->
+            val textView = TextView(this)
+            val responseView = TextView(this)
+            val responseTime = recognisedCandidates.flatMap { it.serviceResults }.firstOrNull { it.provider == title }?.identificationTime
+            textView.text = title
+            responseView.text = responseTime.toString()
+            val params = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                1.0f
+            )
+            textView.layoutParams = params
+            responseView.layoutParams = params
+
+            headerItem.probabilityView.addView(textView)
+            responseTimesItem.probabilityView.addView(responseView)
+            responseTimesItem.fullNameView.text = getString(R.string.response_time)
+        }
+
+        var adapter = VisitorListAdapter(this, this, titles)
         rvVisitorList.adapter = adapter
         rvVisitorList.layoutManager = LinearLayoutManager(this)
 
-        val recognisedCandidates = intent.extras?.get(CANDIDATES_EXTRA) as List<RecognisedCandidate>
         adapter.setVisitors(recognisedCandidates)
 
 
