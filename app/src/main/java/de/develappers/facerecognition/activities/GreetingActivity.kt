6a4c0080d7de0bf26f0a72e8fd45fb3d8729756f
Elena Stepanova <e.stepanova@enveto.de>
2020-05-31 12:59:32 +0200

    write logbook to txt file, return to greeting activity afterwards

--- app/src/main/java/de/develappers/facerecognition/activities/GreetingActivity.kt
+++ app/src/main/java/de/develappers/facerecognition/activities/GreetingActivity.kt
@@ -1,8 +1,10 @@
 package de.develappers.facerecognition.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
+import com.google.gson.Gson
 import de.develappers.facerecognition.*
 import de.develappers.facerecognition.database.FRdb
 import de.develappers.facerecognition.database.dao.VisitorDao
@@ -13,14 +15,20 @@ import de.develappers.facerecognition.serviceAI.RecognitionService
 import de.develappers.facerecognition.serviceAI.ServiceFactory
 import kotlinx.android.synthetic.main.activity_greeting.*
 import kotlinx.coroutines.*
+import java.io.File
+import java.io.Serializable
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.coroutines.CoroutineContext
 
 class GreetingActivity : AppCompatActivity() {
 
     private lateinit var visitorDao: VisitorDao
     private var log = LogEntry()
-    private lateinit var serviceProviders:MutableList<RecognitionService>
+    private lateinit var serviceProviders: MutableList<RecognitionService>
+    lateinit var aiJob: Job
+    lateinit var dbJob: Job
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,68 +37,83 @@ class GreetingActivity : AppCompatActivity() {
         val imgPath: String? = intent.extras?.getString(NEW_IMAGE_PATH_EXTRA)
 
         //AI services
-        serviceProviders = ServiceFactory.createAIServices(this,
+        serviceProviders = ServiceFactory.createAIServices(
+            this,
             FaceApp.values
         )
 
         lifecycleScope.launch {
             visitorDao = FRdb.getDatabase(application, this).visitorDao()
-        }
 
-        if (intent.hasExtra(VISITOR_EXTRA)) {
-            val visitor = intent.getSerializableExtra(VISITOR_EXTRA) as Visitor
-            tvGreeting.text = getString(R.string.greeting, visitor.lastName)
+            if (intent.hasExtra(VISITOR_EXTRA)) {
+                val visitor = intent.getSerializableExtra(VISITOR_EXTRA) as Visitor
+                tvGreeting.text = getString(R.string.greeting, visitor.lastName)
 
-            lifecycleScope.launch {
                 //register the visitor within AI services and set visitor service ID in the database
-                registerVisitorInAIServices(visitor)
-                //save new visitor in database, get visitor id
-                val newVisitorId = visitorDao.insert(visitor)
-                registerNewVisitor(newVisitorId.toString())
+                dbJob = lifecycleScope.launch {
+                    registerVisitorInAIServices(visitor)
+                    //save new visitor in database, get visitor id
+                    val newVisitorId = visitorDao.insert(visitor)
+                    registerNewVisitor(newVisitorId.toString())
+                }
 
                 //train the database with the new image, if needed
                 serviceProviders.forEach {
                     it.train()
                 }
+
             }
+
+            if (intent.hasExtra(RECOGNISED_CANDIDATE_EXTRA)) {
+                val recognisedCandidate = intent.getSerializableExtra(RECOGNISED_CANDIDATE_EXTRA) as RecognisedCandidate
+                tvGreeting.text = getString(R.string.greeting, recognisedCandidate.visitor.lastName)
+
+                aiJob = lifecycleScope.launch {
+                    addNewPhotoToAIServices(imgPath, recognisedCandidate.visitor)
+                }
+                dbJob = lifecycleScope.launch {
+                    addNewPhotoPathToDatabase(imgPath, recognisedCandidate.visitor)
+
+                }
+                registerRepeatingVisitor(recognisedCandidate)
+                aiJob.join()
+            }
+
+
+            dbJob.join()
+            navigateToStart()
+
         }
-
-        if (intent.hasExtra(RECOGNISED_CANDIDATE_EXTRA)) {
-            val recognisedCandidate = intent.getSerializableExtra(RECOGNISED_CANDIDATE_EXTRA) as RecognisedCandidate
-            tvGreeting.text = getString(R.string.greeting, recognisedCandidate.visitor.lastName)
-
-            lifecycleScope.launch {
-                addNewPhotoToAIServices(imgPath, recognisedCandidate.visitor)
-            }
-            lifecycleScope.launch {
-                addNewPhotoPathToDatabase(imgPath, recognisedCandidate.visitor)
-
-            }
-
-            registerRepeatingVisitor(recognisedCandidate)
-            println("Logged visit: $log")
-            //TODO: write to logbook file
-        }
-
 
     }
 
-    fun registerNewVisitor(visitorId: String) {
+    private fun navigateToStart() {
+        intent = Intent(this@GreetingActivity, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun registerNewVisitor(visitorId: String) {
         log.timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
         log.trueVisitorId = visitorId
+        writeToLogbook()
 
     }
 
-    fun registerRepeatingVisitor(candidate: RecognisedCandidate) {
+    private fun registerRepeatingVisitor(candidate: RecognisedCandidate) {
         log.timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
         log.serviceResults = candidate.serviceResults
         log.trueVisitorId = candidate.visitor.visitorId.toString()
-
+        writeToLogbook()
     }
 
-    suspend fun addNewPhotoToAIServices(imgPath: String?, visitor: Visitor) = withContext(Dispatchers.IO) {
+    private fun writeToLogbook() {
+        println("Logged visit: ${Gson().toJson(log)}")
+        File(FaceApp.storageDirectory, "logbook.txt").appendText("${Gson().toJson(log)}\n")
+    }
+
+    private suspend fun addNewPhotoToAIServices(imgPath: String?, visitor: Visitor) = withContext(Dispatchers.IO) {
         // withContext waits for all children coroutines
-        serviceProviders.forEach {service ->
+        serviceProviders.forEach { service ->
             if (service.isActive) {
                 launch {
                     imgPath?.let { service.addNewImage(VISITORS_GROUP_ID, it, visitor) }
@@ -99,12 +122,12 @@ class GreetingActivity : AppCompatActivity() {
         }
     }
 
-    suspend fun addNewPhotoPathToDatabase(imgPath: String?, visitor: Visitor) {
-       imgPath?.let { visitor.imgPaths.add(it) }
+    private suspend fun addNewPhotoPathToDatabase(imgPath: String?, visitor: Visitor) {
+        imgPath?.let { visitor.imgPaths.add(it) }
         visitorDao.updateVisitor(visitor)
     }
 
-    suspend fun registerVisitorInAIServices(visitor: Visitor) = withContext(Dispatchers.IO) {
+    private suspend fun registerVisitorInAIServices(visitor: Visitor) = withContext(Dispatchers.IO) {
         // withContext waits for all children coroutines
         serviceProviders.forEach {
             if (it.isActive) {
