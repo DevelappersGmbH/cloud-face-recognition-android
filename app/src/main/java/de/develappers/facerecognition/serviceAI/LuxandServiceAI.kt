05f10c78979b1e3e99a999094a79949544d919ed
Elena Stepanova <e.stepanova@enveto.de>
2020-06-01 13:57:45 +0200

    add a multipart image call

--- app/src/main/java/de/develappers/facerecognition/serviceAI/LuxandServiceAI.kt
+++ app/src/main/java/de/develappers/facerecognition/serviceAI/LuxandServiceAI.kt
@@ -3,23 +3,27 @@ package de.develappers.facerecognition.serviceAI
 import android.content.Context
 import android.net.Uri
 import android.util.Log
-import com.microsoft.projectoxford.face.contract.CreatePersonResult
-import de.develappers.facerecognition.*
+import de.develappers.facerecognition.FaceApp
+import de.develappers.facerecognition.FaceApp.Companion.galleryFolder
+import de.develappers.facerecognition.R
 import de.develappers.facerecognition.database.model.entities.Visitor
-import de.develappers.facerecognition.retrofit.FaceApi
-import de.develappers.facerecognition.serviceAI.faceServiceAI.model.*
 import de.develappers.facerecognition.serviceAI.luxandServiceAI.model.AddFaceToPersonResponse
 import de.develappers.facerecognition.serviceAI.luxandServiceAI.model.CreatePersonResponse
+import de.develappers.facerecognition.serviceAI.luxandServiceAI.model.LuxandFace
 import de.develappers.facerecognition.utils.ImageHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.io.Serializable
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import java.io.File
 
 
 class LuxandServiceAI(
     val context: Context,
     override var provider: String,
-    override var isActive: Boolean): RecognitionService {
+    override var isActive: Boolean
+) : RecognitionService {
 
     val luxandApi = FaceApp.luxandApi
 
@@ -39,21 +43,33 @@ class LuxandServiceAI(
     override suspend fun addNewVisitorToDatabase(personGroupId: String, imgUri: String, visitor: Visitor) {
         //step 2
         val createPersonResponse = addPersonToGroup(personGroupId)
-        luxandAddFaceToPerson()
-        setServiceId(visitor, createPersonResult.personId.toString())
+        setServiceId(visitor, createPersonResponse.id.toString())
+        addNewImage(personGroupId, imgUri, visitor)
 
     }
 
     override suspend fun addNewImage(personGroupId: String, imgUri: String, visitor: Visitor) {
-        addNewVisitorToDatabase(personGroupId, imgUri, visitor)
+        val body = createBodyPart(imgUri)
+        luxandAddFaceToPerson(visitor.luxandId.toInt(), body)
+    }
+
+    fun createBodyPart(imgUri: String): MultipartBody.Part{
+        val photoFile = File(galleryFolder, imgUri.substringAfter("${context.getString(R.string.app_name)}/"))
+
+        val body = MultipartBody.Part.createFormData(
+            "photo", photoFile.name, RequestBody.create(
+                MediaType.parse("multipart/form-data"),
+                photoFile
+            )
+        )
+        return body
     }
 
     override suspend fun identifyVisitor(personGroupId: String, imgUri: String): List<Any> {
-        var imageUri = Uri.parse(imgUri)
-        val imgBitmap = ImageHelper.loadSizeLimitedBitmapFromUri(imageUri, context)
-        val imgString = ImageHelper.encodeImage(imgBitmap)
-        val searchFaceResponse = faceSearch(imgString) as SearchResponse
-        return searchFaceResponse.results as List<Any>;
+        val body = createBodyPart(imgUri)
+        val searchFaceResponse = luxandFaceSearch(body) as List<Any>
+
+        return searchFaceResponse
 
     }
 
@@ -67,9 +83,9 @@ class LuxandServiceAI(
         }
 
 
-    suspend fun luxandAddFaceToPerson(luxandId: String?, photoFile: String?): Any?  =
+    suspend fun luxandAddFaceToPerson(luxandId: Int?, photoFile: MultipartBody.Part): Any? =
         withContext(Dispatchers.IO) {
-            var result : AddFaceToPersonResponse? = null
+            var result: AddFaceToPersonResponse? = null
             try {
                 result = luxandApi.addFaceToPerson(luxandId, photoFile)
                 result
@@ -78,30 +94,31 @@ class LuxandServiceAI(
             }
         }
 
-    suspend fun luxandFaceSearch(photoFile: String?): Any? =
+    suspend fun luxandFaceSearch(photoFile: MultipartBody.Part): Any =
         withContext(Dispatchers.IO) {
+            var result: List<LuxandFace> = mutableListOf()
             try {
-                luxandApi.search(photoFile)
+                result = luxandApi.search(photoFile)
+                result
             } catch (e: Exception) {
                 Log.d("Luxand search: ", e.toString())
             }
         }
 
 
-    override fun setServiceId(visitor: Visitor, id: String){
+    override fun setServiceId(visitor: Visitor, id: String) {
         //should be an array, not used
         //user defined id in the service is set to the last imgPath, look up in the local db in imgpaths[]
         visitor.faceId = id
     }
 
-
     override fun defineLocalIdPath(candidate: Any): String {
-        candidate as SearchFaceResult
-        return "${FaceApp.galleryFolder}/${candidate.user_id}"
+        candidate as LuxandFace
+        return candidate.id.toString()
     }
 
     override fun defineConfidenceLevel(candidate: Any): Double {
-        candidate as SearchFaceResult
-        return candidate.confidence / 100.0
+        candidate as LuxandFace
+        return candidate.probability
     }
 }
